b9cb700262e97bc78772a455352ae6f7e354593d
Mohamed Hassan <79491085+mohamed-h-rabie@users.noreply.github.com>
2024-07-28 16:42:01 +0300

    generatestaticparams make cabin[id] static

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -5,6 +5,14 @@ export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
 }
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
+}
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
